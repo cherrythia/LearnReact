a7c2334809896fe25d4cf84f00081933af4be212
Terry Chia <chiawz@hotmail.com>
2023-05-25 01:14:32 +0800

    feat: complete tic-tac-toe

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -4,9 +4,7 @@ function Square({value, onSquareClick}) {
   return <button className="square" onClick={onSquareClick}>{value}</button>;
 }
 
-export default function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null)) // [state, setState
-  const [isNext, setIsNext] = useState(true) // [state, setState
+function Board({isNext, squares, onPlay}) {
 
   const winner = calculateWinner(squares);
   
@@ -25,8 +23,7 @@ export default function Board() {
     } else {
       nextSquares[i] = "O";
     } 
-    setIsNext(!isNext);
-    setSquares(nextSquares);
+    onPlay(nextSquares)
   }
 
   function calculateWinner(squares) {
@@ -68,5 +65,47 @@ export default function Board() {
         <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
       </div>
     </>
+  );
+}
+
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]) // [state, setState
+  const [currentMove, setCurrentMove] = useState(0) 
+  const isNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove]
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove)
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board isNext={isNext} squares={currentSquares} onPlay={handlePlay}/>
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
   );
 }
